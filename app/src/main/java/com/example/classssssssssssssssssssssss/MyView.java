c872fe138a12d8e276f1ac1b58544c65651a805b
Enotich <enotichenotov195@gmail.com>
2023-01-20 23:42:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/classssssssssssssssssssssss/MyView.java
@@ -0,0 +1,91 @@
+package com.example.classssssssssssssssssssssss;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.Pair;
+import androidx.annotation.NonNull;
+
+//import java.awt.Paint;
+public class MyView extends SurfaceView implements SurfaceHolder.Callback {
+        private SurfaceHolder surfaceHolder;
+        private final MyThread thread;
+        private Two circlecoor;
+        private Two circleto;
+    public MyView(Context context) {
+            super(context);
+            circlecoor= new Two(500,500);
+            circleto = new Two(500,500);
+            getHolder().addCallback(this);
+            thread = new MyThread();
+        }
+
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+            this.surfaceHolder = surfaceHolder;
+            thread.start();
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {}
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {}
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            circleto.x=event.getX();
+            circleto.y=event.getY();
+            invalidate();
+            return super.onTouchEvent(event);
+        }
+        public void move(){
+            float dx = circleto.x-circlecoor.x;
+            float dy = circleto.y - circlecoor.y;
+            float len = (float) Math.sqrt(dx*dx+dy*dy);
+            float cosd = dx/len;
+            float sind = dy/len;
+            if(len>40f){
+                circlecoor.x+=40f*cosd;
+                circlecoor.y+=40f*sind;
+            }
+            else{
+                circlecoor.x = circleto.x;
+                circlecoor.y=circleto.y;
+            }
+        }
+        private class MyThread extends Thread {
+            boolean running = true;
+            @Override
+            public void run() {
+                Canvas canvas;
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                while(running){
+                    canvas = surfaceHolder.lockCanvas();
+                    try {
+                        sleep(50);
+                        move();
+                        canvas.drawColor(Color.BLUE);
+                        canvas.drawCircle(circlecoor.x,circlecoor.y,50f,paint);;
+
+                    } catch (InterruptedException e) {}
+                    finally {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                }
+            }
+        }
+    }
+    class Two{
+        public float x;
+        public float y;
+        public Two(float x,float y) {
+            this.x = x;
+            this.y=y;
+        }
+    }
+
